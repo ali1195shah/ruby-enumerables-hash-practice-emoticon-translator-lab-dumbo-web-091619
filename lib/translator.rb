f58cf1c3f9dfed0cdb0a2e3b49711d5a9afd6c9b
Ali Shah <ali1195shah@gmail.com>
2019-08-28 23:27:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
+require "yaml"
+emoticons = YAML.load_file("./lib/emoticons.yml")
 
-def load_library
+def load_library(emoticons)
   # code goes here
+  hash = Hash.new()
+  hash[emoticons]
 end
 
 def get_japanese_emoticon
