dcd64ebc6a3a08e1cdf2fc6d4333824d2818594b
Ali Shah <ali1195shah@gmail.com>
2019-08-28 23:36:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,9 @@ hash = YAML.load_file("./lib/emoticons.yml")
 
 def load_library(hash)
   # code goes here
-  emoticons = {"get_meaning" => }
+  emoticons = {"get_meaning" => {},
+               "get_emoji" => {}
+  }
 end
 
 def get_japanese_emoticon
